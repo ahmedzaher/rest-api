8374e5b58cc76d09c75623ae32929388997d667a
Ahmed <ahmedzaher54@gmail.com>
2019-05-13 13:34:19 +0200

    first

---
+++ src/main/java/com/example/demo/RestAPI.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class RestAPI {
+
+    @GetMapping
+    public Object get() {
+        HashMap m = new HashMap();
+        m.put("message", "Hello ");
+        return m;
+    }
+}
