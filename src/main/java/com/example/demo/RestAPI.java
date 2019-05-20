873788343325f22b033553fca1c04fc43b204adc
Ahmed <ahmedzaher54@gmail.com>
2019-05-20 14:48:12 +0200

    zaher | f2

--- src/main/java/com/example/demo/RestAPI.java
+++ src/main/java/com/example/demo/RestAPI.java
@@ -11,7 +11,7 @@ public class RestAPI {
     @GetMapping
     public Object get() {
         HashMap m = new HashMap();
-        m.put("message", "Hello f2");
+        m.put("message", "Hello f2 ");
         return m;
     }
 }
